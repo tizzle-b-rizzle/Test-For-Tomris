277004aa13fa987c5418a199e8ca2d89d0eaf4c0
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2021-06-01 13:22:59 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function jigsawHighlight() {
+  test1 = document.getElementById('tl-one-one-outer');
+  test1.style.backgroundColor = "red";
+  test2 = document.getElementById('tl-one-one-middle');
+  test2.style.backgroundColor = "red";
+  test3 = document.getElementById('tl-one-one-inner');
+  test3.style.backgroundColor = "red";
+}
+function jigsawHighlight2() {
+    test1 = document.getElementById('tl-one-one-outer');
+    test1.style.backgroundColor = "blue";
+    test2=document.getElementById("rs-one-one-outer");
+    test2.style.backgroundColor="red"
+}
